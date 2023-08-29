6835bff4f3edba2290a8c844fd2b71626c3a1b17
Dennis van Loon <>
2023-08-29 11:14:43 +0200

    Finish section 6

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import {Recipe} from "../recipe.model";
 
@@ -9,12 +9,17 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is just a test', 'https://www.acouplecooks.com/wp-content/uploads/2020/10/Cauliflower-Stir-Fry-011.jpg'),
-    new Recipe('A Test Recipe', 'This is just a test', 'https://www.acouplecooks.com/wp-content/uploads/2020/10/Cauliflower-Stir-Fry-011.jpg')
+    new Recipe('A Second Recipe', 'This is just a second test', 'https://www.acouplecooks.com/wp-content/uploads/2020/10/Cauliflower-Stir-Fry-011.jpg')
   ];
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipeEl: Recipe) {
+    this.recipeWasSelected.emit(recipeEl);
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ShoppingEditComponent {
 
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
