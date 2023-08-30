75d65b49de72075505d5ebf230be1dd5b22743a6
Dennis van Loon <>
2023-08-30 16:23:23 +0200

    Finish section 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {UserService} from "./users.service";
+import {CounterService} from "./counter.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService, CounterService]
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {InactiveUsersComponent} from "./inactive-users/inactive-users.component";
+import {ActiveUsersComponent} from "./active-users/active-users.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InactiveUsersComponent,
+    ActiveUsersComponent
   ],
   imports: [
     BrowserModule
