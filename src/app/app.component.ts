4d980a50bcd0a55af1e989a723c41586bcb327c9
Dennis van Loon <>
2023-09-08 15:16:59 +0200

    finish chapter 15 exercise 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormControlName, FormGroup, NgForm, Validators} from "@angular/forms";
+import {CustomValidators} from "./custom-validators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  subscriptions = ['Basic', 'Advanced', 'Pro'];
-  selectedSubscription = 'Advanced';
-  @ViewChild('signupForm', {static: false}) sgnForm: NgForm;
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
 
-  onSubmit() {
-    console.log(this.sgnForm.value);
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required /*, CustomValidators.invalidProjectName*/], CustomValidators.asyncInvalidProjectName),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical', [Validators.required, Validators.email]),
+
+    })
   }
+
+  onSaveProject() {
+    console.log(this.projectForm.value)
+  }
+
+  // subscriptions = ['Basic', 'Advanced', 'Pro'];
+  // selectedSubscription = 'Advanced';
+  // @ViewChild('signupForm', {static: false}) sgnForm: NgForm;
+
+  // onSubmit() {
+  //   console.log(this.sgnForm.value);
+  // }
 }
